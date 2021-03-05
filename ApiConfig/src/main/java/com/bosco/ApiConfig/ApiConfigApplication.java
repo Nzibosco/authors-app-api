67890ffd73ca78a24077df9a8b46cad903dc9faf
Nzibosco <nzibosco@gmail.com>
2021-03-05 18:35:57 -0500

    adding dependencies for config server

---
+++ ApiConfig/src/main/java/com/bosco/ApiConfig/ApiConfigApplication.java
@@ -0,0 +1,13 @@
+package com.bosco.ApiConfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiConfigApplication.class, args);
+	}
+
+}
---
+++ ApiConfig/src/test/java/com/bosco/ApiConfig/ApiConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bosco.ApiConfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
