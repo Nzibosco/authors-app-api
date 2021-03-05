8885829bd67e9dc7e905b8a5a72c8bdfde56d3ae
Nzibosco <nzibosco@gmail.com>
2021-03-05 17:56:32 -0500

    reformatting the config server

--- authorsApiConfig/src/main/java/com/bosco/authorsApiConfig/AuthorsApiConfigApplication.java
+++ authorsApiConfig/src/main/java/com/bosco/authorsApiConfig/AuthorsApiConfigApplication.java
@@ -2,8 +2,10 @@ package com.bosco.authorsApiConfig;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class AuthorsApiConfigApplication {
 
 	public static void main(String[] args) {
