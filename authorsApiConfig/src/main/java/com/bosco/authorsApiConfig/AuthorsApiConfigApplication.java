c86256a57e8afeccc38f77653503b7d6af274834
Nzibosco <nzibosco@gmail.com>
2021-03-05 17:48:40 -0500

    creating a configuration server

---
+++ authorsApiConfig/src/main/java/com/bosco/authorsApiConfig/AuthorsApiConfigApplication.java
@@ -0,0 +1,13 @@
+package com.bosco.authorsApiConfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorsApiConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthorsApiConfigApplication.class, args);
+	}
+
+}
---
+++ authorsApiConfig/src/test/java/com/bosco/authorsApiConfig/AuthorsApiConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bosco.authorsApiConfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorsApiConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
